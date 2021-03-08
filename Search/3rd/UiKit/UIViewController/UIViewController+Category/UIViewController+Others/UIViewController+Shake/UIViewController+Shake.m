96bdab157ab6188d4f534d2ad43c9f4b58f8957b
Jobs <lg295060456@gmail.com>
2021-03-08 10:04:32 +0700

    no message

--- Search/3rd/UiKit/UIViewController/UIViewController+Category/UIViewController+Others/UIViewController+Shake/UIViewController+Shake.m
+++ Search/3rd/UiKit/UIViewController/UIViewController+Category/UIViewController+Others/UIViewController+Shake/UIViewController+Shake.m
@@ -115,12 +115,14 @@ static char *UIViewController_ShakeEnd = "UIViewController_ShakeEnd";
 
 -(void)outputAccelertionData:(CMAcceleration)acceleration{
     //综合3个方向的加速度
-    double accelerameter =sqrt( pow( acceleration.x , 2 ) + pow( acceleration.y , 2 ) + pow( acceleration.z , 2) );
+    double accelerameter = sqrt(pow( acceleration.x , 2 ) + pow( acceleration.y , 2 ) + pow( acceleration.z , 2));
     //当综合加速度大于2.3时，就激活效果（此数值根据需求可以调整，数据越小，用户摇动的动作就越小，越容易激活，反之加大难度，但不容易误触发）
     if (accelerameter > 1.5f) {
         //立即停止更新加速仪（很重要！）
         [self.motionManager stopAccelerometerUpdates];
+        @weakify(self)
         dispatch_async(dispatch_get_main_queue(), ^{
+            @strongify(self)
             //UI线程必须在此block内执行，例如摇一摇动画、UIAlertView之类
             //设置开始摇晃时震动
             [NSObject shake];
